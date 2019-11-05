47c2cc5b1c61734615ca832448a007c377089bf3
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2019-11-05 12:34:48 -0800

    some big boxes

--- ExamplePhysicsServer.lua
+++ ExamplePhysicsServer.lua
@@ -53,9 +53,15 @@ function GameServer.receivers:createMainWorld()
         local fixtureId = self:physics_newFixture(bodyId, shapeId, 1)
     end
 
-    for i = 1, 10 do -- Boxes
+    for i = 1, 10 do -- Small boxes
         local bodyId = self:physics_newBody(worldId, math.random(70, 800 - 70), math.random(0, 300), 'dynamic')
-        local shapeId = self:physics_newRectangleShape(math.random(20, 100), math.random(20, 100))
+        local shapeId = self:physics_newRectangleShape(40, 40)
+        local fixtureId = self:physics_newFixture(bodyId, shapeId, 1)
+    end
+
+    for i = 1, 2 do -- Big boxes
+        local bodyId = self:physics_newBody(worldId, math.random(70, 800 - 70), math.random(0, 300), 'dynamic')
+        local shapeId = self:physics_newRectangleShape(math.random(100, 180), math.random(120, 240))
         local fixtureId = self:physics_newFixture(bodyId, shapeId, 1)
     end
 
